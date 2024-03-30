67484e07144c76714ee85de7236d40f1d73d1b3c
Oviel Felipe Mendoza Pineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-03-29 21:47:22 -0500

    feat: :construction: Menú responsivo.
    Se creó el menu responsivo con sus estilos y animaciones.

--- app/componentes/Header/header.js
+++ app/componentes/Header/header.js
@@ -2,6 +2,7 @@ export class Header extends HTMLElement  {
     constructor() {
         super()
         this.render()
+        this.showSidebar()
     }
     render() {
         this.innerHTML = /* HTML*/ `
@@ -12,6 +13,9 @@ export class Header extends HTMLElement  {
         <div class="name">
             <h1>Felipe <b>Mendoza</b></h1>
         </div>
+        <div class="btn-menu">
+            <i class='bx bx-menu'  ></i>
+        </div>
         <ul class="links">
             <li class="link"><a href="#">Home</a></li>
             <li class="link"><a href="#">Education</a></li>
@@ -21,6 +25,12 @@ export class Header extends HTMLElement  {
         </ul>
     </header>
         ` 
+    }
+    showSidebar() {
+        const links = this.querySelector('.links')
+        const btnMenu = this.querySelector('.btn-menu').addEventListener('click', (e) => {
+            links.classList.toggle('active')
+        })
     }
 }
 customElements.define("custom-header", Header)
